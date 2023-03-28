aa158225c8a1645b8828acae93759699e2b0419d
Anna Soboleva <annasoboleva787@gmail.com>
2023-03-28 17:12:12 +0200

    SNAK-9 еда появляется в рандомном месте при открытии игры

--- food.py
+++ food.py
@@ -1,9 +1,10 @@
 from block import *
 from screen import YELLOW
+import random
 
 class Food:
     color = YELLOW
     block = None
 
-    def __init__(self):
-        self.block = Block(8, 2, self.color)
+    def __init__(self, x=random.randint(0, 9), y=random.randint(0, 9)):
+        self.block = Block(x, y, self.color)
