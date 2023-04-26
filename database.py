572be7a6f18d933fc079911b19d042c6c6ad12a4
Anna Soboleva <annasoboleva787@gmail.com>
2023-04-26 19:27:22 +0200

    SNAK-24 рекорд сохраняется с помощью SQLite

--- database.py
+++ database.py
@@ -1,16 +1,51 @@
+import sqlite3
+import contextlib
+import os
+# Scores:
+# ==================================
+# | Id       | Name     |  Score    |
+# |----------|----------|-----------|
+# | 1        | 'Player1'|    10     |
+# ===================================
+
 class DataBase:
-    def __init__(self, name="test.db"):
+    def __init__(self, name="test2.db"):
         self.filename = name
+        self.insert_query = """INSERT INTO Scores (Name, Score)
+                           VALUES (?, ?); """
+        self.update_query = """UPDATE Scores 
+                               SET Score=?
+                               WHERE Name=?;
+        """
+        if not os.path.exists(self.filename):
+            self.create()
+
+    def create(self):
+        create_table = """CREATE TABLE Scores (
+                   id INTEGER PRIMARY KEY,
+                   Name TEXT,
+                   Score Integer
+                   );
+        """
+        with contextlib.closing(sqlite3.connect(self.filename)) as con:
+            with con:
+                with contextlib.closing(con.cursor()) as cur:
+                    cur.execute(create_table)
+                    cur.execute(self.insert_query, ("Player1", 0))
 
     def load(self):
         result = {}
-        with open(self.filename, "r") as f:
-            result = {"Player1": int(f.read())}
+        with contextlib.closing(sqlite3.connect(self.filename)) as con:
+            with con:
+                with contextlib.closing(con.cursor()) as cur:
+                    cur.execute('SELECT Name, Score FROM Scores WHERE Name="Player1";')
+                    res = cur.fetchone() # -> ('Player1', 10)
+                    result = {"Player1": res[1]}
         return result
 
     def store(self, values):
-        with open(self.filename, "w") as f:
-            f.write(str(values["Player1"]))
-
-    def close(self):
-        pass
+        data = (values["Player1"], "Player1")
+        with contextlib.closing(sqlite3.connect(self.filename)) as con:
+            with con:
+                with contextlib.closing(con.cursor()) as cur:
+                    cur.execute(self.update_query, data)
